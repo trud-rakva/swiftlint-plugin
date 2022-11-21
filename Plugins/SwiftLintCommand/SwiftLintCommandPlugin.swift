f22112e5c35183587e7208b595661bbfd5547bb1
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2022-11-21 17:14:28 +0100

    Added XcodeCommandPlugin
    Signed-off-by: trud-rakva <65568928+trud-rakva@users.noreply.github.com>

--- Plugins/SwiftLintCommand/SwiftLintCommandPlugin.swift
+++ Plugins/SwiftLintCommand/SwiftLintCommandPlugin.swift
@@ -1,14 +1,17 @@
-import Foundation
 import PackagePlugin
 
 @main
 struct SwiftLintCommandPlugin: CommandPlugin {
 
+    /// This entry point is called when operating on a Swift package.
     func performCommand(context: PluginContext, arguments: [String]) throws {
+        print("SwiftLint Command plugin execution for Swift package \(context.package.displayName)")
+        
         let tool = try context.tool(named: "swiftlint")
         let toolURL = URL(fileURLWithPath: tool.path.string)
         var toolArguments = [
-            "lint"
+            "lint",
+            "--cache-path", "\(context.pluginWorkDirectory.string)/cache" ]
         ]
 
         var argumentExtractor = ArgumentExtractor(arguments)
@@ -25,8 +28,6 @@ struct SwiftLintCommandPlugin: CommandPlugin {
             toolArguments.append("--strict")
         }
 
-        toolArguments.append(contentsOf: [ "--cache-path", "\(context.pluginWorkDirectory.string)/cache" ])
-                                  
         let process = try Process.run(toolURL, arguments: toolArguments)
         process.waitUntilExit()
 
@@ -37,3 +38,44 @@ struct SwiftLintCommandPlugin: CommandPlugin {
     }
 
 }
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintCommandPlugin XcodeCommandPlugin {
+
+    /// This entry point is called when operating on an Xcode project.
+    func performCommand(context: XcodePluginContext, arguments: [String]) throws {
+        print("SwifLint Command plugin execution for Xcode project \(context.xcodeProject.displayName)")
+        
+        let tool = try context.tool(named: "swiftlint")
+        let toolURL = URL(fileURLWithPath: tool.path.string)
+        var toolArguments = [
+            "lint",
+            "--cache-path", "\(context.pluginWorkDirectory.string)/cache" ]
+        ]
+
+        var argumentExtractor = ArgumentExtractor(arguments)
+
+        if let configFile = argumentExtractor.extractOption(named: "config").first {
+            toolArguments.append(contentsOf: ["--config", configFile])
+        }
+
+        if let reporter = argumentExtractor.extractOption(named: "reporter").first {
+            toolArguments.append(contentsOf: ["--reporter", reporter])
+        }
+
+        if argumentExtractor.extractFlag(named: "strict") > 0 {
+            toolArguments.append("--strict")
+        }
+
+        let process = try Process.run(toolURL, arguments: toolArguments)
+        process.waitUntilExit()
+
+        if process.terminationStatus != 0 {
+            let msg = "\(process.terminationStatus): \(process.terminationReason)"
+            Diagnostics.error("\(toolURL) command failed: \(msg)")
+        }
+    }
+}
+#endif
