42407672bd7e7e755973dba38233f4cb0fa357b4
trud-rakva <65568928+trud-rakva@users.noreply.github.com>
2022-11-22 01:16:17 +0100

    Update SwiftLintCommandPlugin.swift
    Signed-off-by: trud-rakva <65568928+trud-rakva@users.noreply.github.com>

--- Plugins/SwiftLintCommand/SwiftLintCommandPlugin.swift
+++ Plugins/SwiftLintCommand/SwiftLintCommandPlugin.swift
@@ -3,16 +3,12 @@ import Foundation
 
 @main
 struct SwiftLintCommandPlugin: CommandPlugin {
-
-    /// This entry point is called when operating on a Swift package.
-    func performCommand(context: PluginContext, arguments: [String]) throws {
-        print("SwiftLint Command Plugin execution for Swift package \(context.package.displayName)")
-
-        let tool = try context.tool(named: "swiftlint")
-        let toolURL = URL(fileURLWithPath: tool.path.string)
+    
+    private func runCommand(workDir: String, tool: String, arguments: [String]) throws {
+        let toolURL = URL(fileURLWithPath: tool)
         var toolArguments = [
             "lint",
-            "--cache-path", "\(context.pluginWorkDirectory.string)/cache"
+            "--cache-path", "\(workDir)/cache"
         ]
 
         var argumentExtractor = ArgumentExtractor(arguments)
@@ -29,7 +25,7 @@ struct SwiftLintCommandPlugin: CommandPlugin {
             toolArguments.append("--strict")
         }
 
-        print("Executing: \(toolURL) \(toolArguments)")
+        print("run: \(toolURL) \(toolArguments)")
         let process = try Process.run(toolURL, arguments: toolArguments)
         process.waitUntilExit()
 
@@ -37,6 +33,16 @@ struct SwiftLintCommandPlugin: CommandPlugin {
             let msg = "\(process.terminationStatus): \(process.terminationReason)"
             Diagnostics.error("\(toolURL) command failed: \(msg)")
         }
+    }
+
+    /// This entry point is called when operating on a Swift package.
+    func performCommand(context: PluginContext, arguments: [String]) throws {
+        print("SwiftLint Command Plugin execution for Swift package \(context.package.displayName)")
+        print("context: \(context)")
+        print("arguments: \(arguments)")
+
+        let tool = try context.tool(named: "swiftlint")
+        runCommand(workDir: context.pluginWorkDirectory.string, tool: tool.path.string, arguments: arguments)
     }
 
 }
@@ -49,36 +55,11 @@ extension SwiftLintCommandPlugin: XcodeCommandPlugin {
     /// This entry point is called when operating on an Xcode project.
     func performCommand(context: XcodePluginContext, arguments: [String]) throws {
         print("SwifLint Command Plugin execution for Xcode project \(context.xcodeProject.displayName)")
-
+        print("context: \(context)")
+        print("arguments: \(arguments)")
+        
         let tool = try context.tool(named: "swiftlint")
-        let toolURL = URL(fileURLWithPath: tool.path.string)
-        var toolArguments = [
-            "lint",
-            "--cache-path", "\(context.pluginWorkDirectory.string)/cache"
-        ]
-
-        var argumentExtractor = ArgumentExtractor(arguments)
-
-        if let configFile = argumentExtractor.extractOption(named: "config").first {
-            toolArguments.append(contentsOf: ["--config", configFile])
-        }
-
-        if let reporter = argumentExtractor.extractOption(named: "reporter").first {
-            toolArguments.append(contentsOf: ["--reporter", reporter])
-        }
-
-        if argumentExtractor.extractFlag(named: "strict") > 0 {
-            toolArguments.append("--strict")
-        }
-
-        print("Executing: \(toolURL) \(toolArguments)")
-        let process = try Process.run(toolURL, arguments: toolArguments)
-        process.waitUntilExit()
-
-        if process.terminationStatus != 0 {
-            let msg = "\(process.terminationStatus): \(process.terminationReason)"
-            Diagnostics.error("\(toolURL) command failed: \(msg)")
-        }
+        runCommand(workDir: context.pluginWorkDirectory.string, tool: tool.path.string, arguments: arguments)
     }
 }
 #endif
